996661d810072ba840c102b4b0629201cf0c2554
Junya Takaesu <takaesu_junya@ca-adv.co.jp>
2025-02-13 11:21:51 +0000

    Configure cdk stack to use different vpc cidr based on student id

--- handson/ec2-get-started/app.py
+++ handson/ec2-get-started/app.py
@@ -6,17 +6,23 @@ import os
 
 class MyFirstEc2(core.Stack):
 
-    def __init__(self, scope: core.App, name: str, key_name: str, **kwargs) -> None:
+    def __init__(self, scope: core.App, name: str, key_name: str, student_id: str, **kwargs) -> None:
         super().__init__(scope, name, **kwargs)
+
+        # CIDRを参加者ごとに変える
+        if not 0 < int(student_id) < 255:
+            raise ValueError("student_id は 1 から 254 の間で指定してください")
+
+        vpc_cidr = f"10.{student_id}.0.0/23"
 
         # <1>
         vpc = ec2.Vpc(
-            self, "MyFirstEc2-Vpc",
+            self, f"MyFirstEc2-Vpc-{student_id}",
             max_azs=1,
-            cidr="10.10.0.0/23",
+            cidr=vpc_cidr,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
-                    name="public",
+                    name=f"public-{student_id}",
                     subnet_type=ec2.SubnetType.PUBLIC,
                 )
             ],
@@ -25,9 +31,10 @@ class MyFirstEc2(core.Stack):
 
         # <2>
         sg = ec2.SecurityGroup(
-            self, "MyFirstEc2Vpc-Sg",
+            self, f"MyFirstEc2Vpc-Sg-{student_id}",
             vpc=vpc,
             allow_all_outbound=True,
+            security_group_name=f"ec2-sg-{student_id}"
         )
         sg.add_ingress_rule(
             peer=ec2.Peer.any_ipv4(),
@@ -36,23 +43,27 @@ class MyFirstEc2(core.Stack):
 
         # <3>
         host = ec2.Instance(
-            self, "MyFirstEc2Instance",
+            self, f"MyFirstEc2Instance-{student_id}",
             instance_type=ec2.InstanceType("t2.micro"),
             machine_image=ec2.MachineImage.latest_amazon_linux(),
             vpc=vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
             security_group=sg,
-            key_name=key_name
+            key_name=key_name,
+            instance_name=f"ec2-{student_id}"
         )
 
         # print the server address
-        core.CfnOutput(self, "InstancePublicDnsName", value=host.instance_public_dns_name)
-        core.CfnOutput(self, "InstancePublicIp", value=host.instance_public_ip)
+        core.CfnOutput(self, f"InstancePublicDnsName-{student_id}", 
+                      value=host.instance_public_dns_name)
+        core.CfnOutput(self, f"InstancePublicIp-{student_id}", 
+                      value=host.instance_public_ip)
 
 app = core.App()
 MyFirstEc2(
     app, "MyFirstEc2",
     key_name=app.node.try_get_context("key_name"),
+    student_id=app.node.try_get_context("student_id"),
     env={
         "region": os.environ["CDK_DEFAULT_REGION"],
         "account": os.environ["CDK_DEFAULT_ACCOUNT"],
